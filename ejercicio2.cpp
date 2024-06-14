f15dad544ff2969dfc0af79e2ce64db85a9f36f5
Dante <dalvarezt@unjbg.edu.pe>
2024-06-13 23:13:07 -0500

    Añadiendo una verificación. Se va a comprobar si el precio ingresado es positivo, de lo contrario mandara un mensaje de que es negativo

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,12 +5,16 @@ int main(){
 	double precioCompra, descuento, precioFinal;
 	cout << "Ingrese el precio de su compra: ";
 	cin >> precioCompra;
-	if ( precioCompra > 1000 ){
-		descuento = precioCompra * 0.2;
-		precioFinal = precioCompra - descuento;
-		cout << "Obtuvo un 20% de descuento en su compra, el total a pagar es: " << precioFinal << endl;
+	if ( precioCompra >= 0 ) {
+		if ( precioCompra > 1000 ){
+			descuento = precioCompra * 0.2;
+			precioFinal = precioCompra - descuento;
+			cout << "Obtuvo un 20% de descuento en su compra, el total a pagar es: " << precioFinal << endl;
+		} else {
+			cout << "No obtuvo descuento, el total a pagar es: " << precioCompra << endl;
+		}
 	} else {
-		cout << "No obtuvo descuento, el total a pagar es: " << precioCompra << endl;
+		cout << "Ingreso un precio negativo";
 	}
 	return 0;
 }
