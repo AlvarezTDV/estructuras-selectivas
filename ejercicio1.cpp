a5b3399297a4ac8d12251465a70510a94a1fdecf
Dante <dalvarezt@unjbg.edu.pe>
2024-06-13 23:28:20 -0500

    Mejorando el ejercicio 1 de estructuras selectivas. Se implemento un do while para reiterar en caso de que se hayan ingresado notas negativas

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main() {
 	double nota1, nota2, nota3, promedio;
-	cout << "Ingrese la primera nota: ";
-	cin >> nota1;
-	cout << "Ingrese la segunda nota: ";
-	cin >> nota2;
-	cout << "Ingrese la tercera nota: ";
-	cin >> nota3;
-	if ( nota1 >= 0 and nota2 >= 0 and nota3 >= 0 ) {
-		promedio = ( nota1 + nota2 + nota3 ) / 3;
-		if ( promedio < 70 ) {
-			cout << "Esta desaprobado" << endl;
+	bool verificador = false;
+	do {
+		cout << "Ingrese la primera nota: ";
+		cin >> nota1;
+		cout << "Ingrese la segunda nota: ";
+		cin >> nota2;
+		cout << "Ingrese la tercera nota: ";
+		cin >> nota3;
+		if ( nota1 >= 0 and nota2 >= 0 and nota3 >= 0 ) {
+			promedio = ( nota1 + nota2 + nota3 ) / 3;
+			if ( promedio < 70 ) {
+				cout << "Esta desaprobado" << endl;
+			} else {
+				cout << "Esta aprobado" << endl;
+			}
+			verificador = true;
 		} else {
-			cout << "Esta aprobado" << endl;
+			cout << "Ingreso notas invalidas" << endl;
 		}
-	} else {
-		cout << "Ingreso notas invalidas" << endl;
-	}
+	} while ( verificador != true );
 	return 0;
 }
