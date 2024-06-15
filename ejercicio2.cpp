daadda4b3bedcb179e53ec52fb3b0ee31cb37418
Dante <dalvarezt@unjbg.edu.pe>
2024-06-14 23:16:51 -0500

    Mejora al ejercicio 2 de estructuras selectivas. Se añadió un verificador que revisa si se quiere continuar ingresando precios

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -3,19 +3,24 @@ using namespace std;
 
 int main(){
 	double precioCompra, descuento, precioFinal;
-	cout << "Ingrese el precio de su compra: ";
-	cin >> precioCompra;
-	if ( precioCompra >= 0 ) {
-		if ( precioCompra > 1000 ){
-			descuento = precioCompra * 0.2;
-			precioFinal = precioCompra - descuento;
-			cout << "Obtuvo un 20% de descuento en su compra, el total a pagar es: " << precioFinal << endl;
+	string verificador;
+	do {
+		cout << "Ingrese el precio de su compra: ";
+		cin >> precioCompra;
+		if ( precioCompra >= 0 ) {
+			if ( precioCompra > 1000 ){
+				descuento = precioCompra * 0.2;
+				precioFinal = precioCompra - descuento;
+				cout << "Obtuvo un 20% de descuento en su compra, el total a pagar es: " << precioFinal << endl;
+			} else {
+				cout << "No obtuvo descuento, el total a pagar es: " << precioCompra << endl;
+			}
 		} else {
-			cout << "No obtuvo descuento, el total a pagar es: " << precioCompra << endl;
+			cout << "Ingreso un precio negativo" << endl;
 		}
-	} else {
-		cout << "Ingreso un precio negativo";
-	}
+		cout << "Desea continuar ingresando precios (si o no): ";
+		cin >> verificador;
+	} while ( verificador != "no" );
 	return 0;
 }
 
