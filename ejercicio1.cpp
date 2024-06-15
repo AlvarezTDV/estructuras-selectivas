67f977629eba1cb23576b13d1de3c8c8e7622572
Dante <dalvarezt@unjbg.edu.pe>
2024-06-14 22:40:07 -0500

    Mejora al ejercicio 1 de estructuras selectivas. Se agregó una condición para evitar que una nota ingresada sea mayor que 100

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,13 +5,13 @@ int main() {
 	double nota1, nota2, nota3, promedio;
 	bool verificador = false;
 	do {
-		cout << "Ingrese la primera nota: ";
+		cout << "Ingrese la primera nota (Nota de 0 a 100 puntos): ";
 		cin >> nota1;
-		cout << "Ingrese la segunda nota: ";
+		cout << "Ingrese la segunda nota (Nota de 0 a 100 puntos): ";
 		cin >> nota2;
-		cout << "Ingrese la tercera nota: ";
+		cout << "Ingrese la tercera nota (Nota de 0 a 100 puntos): ";
 		cin >> nota3;
-		if ( nota1 >= 0 and nota2 >= 0 and nota3 >= 0 ) {
+		if ( nota1 >= 0 && nota2 >= 0 && nota3 >= 0 && nota1 <= 100 && nota2 <= 100 && nota3 <= 100 ) {
 			promedio = ( nota1 + nota2 + nota3 ) / 3;
 			if ( promedio < 70 ) {
 				cout << "Esta desaprobado" << endl;
