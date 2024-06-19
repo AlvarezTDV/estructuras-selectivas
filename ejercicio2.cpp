f7dc34110ebcf07172bfbb835e566e7238a54b4f
Dante <dalvarezt@unjbg.edu.pe>
2024-06-18 23:45:40 -0500

    Mejora al ejercicio 2 de estructuras selectivas, se mejoró la interfaz del programa, contiene un título y un mensaje de despedida al terminar el programa

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,23 +4,34 @@ using namespace std;
 int main(){
 	double precioCompra, descuento, precioFinal;
 	string verificador;
+	cout << "--------------------------------------------------" << endl;
+	cout << "        Programa para implementar descuento       " << endl;
+	cout << "--------------------------------------------------" << endl;
 	do {
-		cout << "Ingrese el precio de su compra: ";
+		cout << "Ingrese el precio de su compra => ";
 		cin >> precioCompra;
+		cout << "--------------------------------------------------" << endl;
 		if ( precioCompra >= 0 ) {
 			if ( precioCompra > 1000 ){
 				descuento = precioCompra * 0.2;
 				precioFinal = precioCompra - descuento;
-				cout << "Obtuvo un 20% de descuento en su compra, el total a pagar es: " << precioFinal << endl;
+				cout << "Obtuvo un 20% de descuento en su compra     " << endl;
+				cout << "El total a pagar es => " << precioFinal << endl;
+				cout << "--------------------------------------------------" << endl;
 			} else {
-				cout << "No obtuvo descuento, el total a pagar es: " << precioCompra << endl;
+				cout << "No obtuvo descuento, el total a pagar es => " << precioCompra << endl;
+				cout << "--------------------------------------------------" << endl;
 			}
 		} else {
-			cout << "Ingreso un precio negativo" << endl;
+			cout << "         Ingreso un precio negativo            " << endl;
+			cout << "--------------------------------------------------" << endl;
 		}
-		cout << "Desea continuar ingresando precios (si o no): ";
+		cout << "Desea continuar ingresando precios (si o no) => ";
 		cin >> verificador;
+		cout << "--------------------------------------------------" << endl;
 	} while ( verificador != "no" );
+	cout << "         Gracias por usar este programa           " << endl;
+	cout << "--------------------------------------------------" << endl;
 	return 0;
 }
 
